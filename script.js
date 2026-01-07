5a51284bb9c55566a5acfc6e1908192cb42feff0
sarvottamji31-hub <sarvottamji31@gmail.com>
2026-01-07 19:19:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-/* Search */
 const input = document.getElementById("searchInput");
+
 if (input) {
   input.addEventListener("keyup", () => {
     const value = input.value.toLowerCase();
-    document.querySelectorAll(".feature, .exam-card, .list-card")
+    document.querySelectorAll(".feature,.exam-card")
       .forEach(el => {
         el.style.display = el.innerText.toLowerCase().includes(value)
           ? "block" : "none";
@@ -11,12 +11,10 @@ if (input) {
   });
 }
 
-/* Dark Mode */
 function toggleDark() {
   document.body.classList.toggle("dark");
   localStorage.setItem("dark",
-    document.body.classList.contains("dark")
-  );
+    document.body.classList.contains("dark"));
 }
 
 if (localStorage.getItem("dark") === "true") {
