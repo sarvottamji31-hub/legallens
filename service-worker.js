c35db8a2b9d733bfb2dd06b667d823682a2a0b24
sarvottamji31-hub <sarvottamji31@gmail.com>
2026-01-07 18:09:58 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,7 @@
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("legallens").then(cache => {
-      return cache.addAll([
-        "index.html",
-        "style.css",
-        "script.js"
-      ]);
-    })
+    caches.open("legallens").then(cache =>
+      cache.addAll(["index.html","style.css","script.js"])
+    )
   );
 });
