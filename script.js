549c5979ceac82a9e713b57c9f99a3292224f32f
sarvottamji31-hub <sarvottamji31@gmail.com>
2026-01-07 19:00:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-function showAlert() {
-  alert("Welcome to LegalLens! More features coming soon.");
-}
-
-console.log("LegalLens website loaded successfully.");
+/* Search */
 const input = document.getElementById("searchInput");
-
 if (input) {
   input.addEventListener("keyup", () => {
     const value = input.value.toLowerCase();
-    document.querySelectorAll(".feature, .list-card").forEach(el => {
-      el.style.display = el.innerText.toLowerCase().includes(value)
-        ? "block"
-        : "none";
-    });
+    document.querySelectorAll(".feature, .exam-card, .list-card")
+      .forEach(el => {
+        el.style.display = el.innerText.toLowerCase().includes(value)
+          ? "block" : "none";
+      });
   });
 }
+
+/* Dark Mode */
 function toggleDark() {
   document.body.classList.toggle("dark");
   localStorage.setItem("dark",
